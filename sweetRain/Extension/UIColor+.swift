e4400885abd8d2bb1c342866cf0fe1aa7140733e
p2glet <vviian.2@gmail.com>
2023-12-05 18:01:53 +0900

    [FEAT] : added customDarkblue

--- sweetRain/Extension/UIColor+.swift
+++ sweetRain/Extension/UIColor+.swift
@@ -9,4 +9,5 @@ import UIKit
 
 extension UIColor {
     static let customSkyblue = UIColor(red: 219, green: 229, blue: 241, alpha: 100)
+    static let customDarkblue = UIColor(red: 176, green: 205, blue: 232, alpha: 100)
 }
