d590c9c5e0c7547a24842310c29e7d516489ddd9
p2glet <vviian.2@gmail.com>
2023-12-05 18:00:58 +0900

    [FEAT] : added customSkyblue

---
+++ sweetRain/Extension/UIColor+.swift
@@ -0,0 +1,12 @@
+//
+//  UIColor+.swift
+//  sweetRain
+//
+//  Created by JINHUN CHOI on 2023/12/05.
+//
+
+import UIKit
+
+extension UIColor {
+    static let customSkyblue = UIColor(red: 219, green: 229, blue: 241, alpha: 100)
+}
