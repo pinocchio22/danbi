f54062d61dbb8b19fd99e2801ed82f30112717f1
p2glet <vviian.2@gmail.com>
2023-12-07 22:34:35 +0900

    [FEAT] : created NetworkManager

---
+++ sweetRain/Secret/KEY.swift
@@ -0,0 +1,12 @@
+//
+//  KEY.swift
+//  sweetRain
+//
+//  Created by JINHUN CHOI on 2023/12/07.
+//
+
+import Foundation
+
+enum Key: String {
+    case apiKey = "d800fe6a5ba7206df395b13ece10adee"
+}
--- sweetRain/Service/NetworkService.swift
+++ sweetRain/Service/NetworkService.swift
@@ -6,3 +6,81 @@
 //
 
 import Foundation
+
+import SnapKit
+import Alamofire
+
+class NetworkService {
+    static let networkService = NetworkService()
+    
+    func getCurrentWeather(lat: Double, lon: Double, completion: @escaping (TodayWeather?) -> Void) {
+        let apiUrl = "https://api.openweathermap.org/data/2.5/weather?lat=\(lat)&lon=\(lon)&appid=\(Key.apiKey.rawValue)&celsius&lang=kr"
+        AF.request(apiUrl).responseDecodable(of: TodayWeather.self) { response in
+            switch response.result {
+            case .success(let currentWeather):
+                completion(currentWeather)
+            case .failure(let error):
+                print("API 요청 실패: \(error)")
+                completion(nil)
+            }
+        }
+    }
+    
+    func getCurrentWeather(cityName: String, completion: @escaping (TodayWeather?) -> Void) {
+        let apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=\(cityName)&appid=\(Key.apiKey.rawValue)&celsius&lang=kr"
+        AF.request(apiUrl).responseDecodable(of: TodayWeather.self) { response in
+            switch response.result {
+            case .success(let currentWeather):
+                completion(currentWeather)
+            case .failure(let error):
+                print("API 요청 실패: \(error)")
+                completion(nil)
+            }
+        }
+    }
+    
+    func getWeeklyWeather(lat: Double, lon: Double, completion: @escaping ([WeeklyWeather]?) -> Void) {
+        let apiUrl = "https://api.openweathermap.org/data/2.5/forecast?lat=\(lat)&lon=\(lon)&appid=\(Key.apiKey.rawValue)&celsius&lang=kr"
+        
+        var weatherList = [WeeklyWeather]()
+        
+        AF.request(apiUrl).responseDecodable(of: WeekWeather.self) { response in
+            switch response.result {
+            case .success(let weeklyWeather):
+                completion(weatherList)
+            case .failure(let error):
+                print("API 요청 실패: \(error)")
+                completion(nil)
+            }
+        }
+    }
+    
+    func getWeeklyWeather(cityName: String, completion: @escaping ([WeeklyWeather]?) -> Void) {
+        let apiUrl = "https://api.openweathermap.org/data/2.5/forecast?q=\(cityName)&appid=\(Key.apiKey.rawValue)&celsius&lang=kr"
+        
+        var weatherList = [WeeklyWeather]()
+        // API 요청 및 디코딩
+        AF.request(apiUrl).responseDecodable(of: WeekWeather.self) { response in
+            switch response.result {
+            case .success(let weeklyWeather):
+                completion(weatherList)
+            case .failure(let error):
+                print("API 요청 실패: \(error)")
+                completion(nil)
+            }
+        }
+    }
+    
+    func getIcon(iconCode: String, completion: @escaping (Data?) -> Void) {
+        let iconURLString = "https://openweathermap.org/img/w/\(iconCode).png"
+        DispatchQueue.global().async {
+            if let iconURL = URL(string: iconURLString) {
+                if let data = try? Data(contentsOf: iconURL) {
+                    completion(data)
+                } else {
+                    completion(nil)
+                }
+            }
+        }
+    }
+}
